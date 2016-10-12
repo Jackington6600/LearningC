a952d40b0bc846c539e006227363364aa9465a4a
Jack Morrison <jack.morrison6600@gmail.com>
2016-10-12 23:10:52 +0100

    Modified Comments

--- LearningC/Celcius2Fahrenheit.cpp
+++ LearningC/Celcius2Fahrenheit.cpp
@@ -4,9 +4,14 @@ using namespace std;
 //Converts a double in Celcius to Fahrenheit
 void c2f() {
 	double c, f;
+
+	//Take user input
 	cout << "Enter a temp in Celcius: ";
 	cin >> c;
 
+	//Calculate
 	f = c*1.8 + 32;
+
+	//Print result
 	cout << c << " degrees Celcius in Fahrenheit is " << f << endl;
 }
